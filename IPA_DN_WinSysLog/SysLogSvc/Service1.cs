cbc00cadb9ef247ea72e31905837f5b80f958f78
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2023-03-05 16:19:29 +0900

    完成！？

--- IPA_DN_WinSysLog/SysLogSvc/Service1.cs
+++ IPA_DN_WinSysLog/SysLogSvc/Service1.cs
@@ -1,27 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
 using System.Text;
+using SysLog;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.ServiceProcess;
+using System.Configuration.Install;
+using System.ComponentModel;
 
-namespace SysLogSvc
+namespace SysLogApp
 {
-    public partial class Service1 : ServiceBase
+    [RunInstaller(true)]
+    public class SysLogServiceInstaller : Installer
     {
-        public Service1()
+        public SysLogServiceInstaller()
         {
-            InitializeComponent();
+            ServiceProcessInstaller proc = new ServiceProcessInstaller();
+            ServiceInstaller svc = new ServiceInstaller();
+
+            proc.Account = ServiceAccount.LocalSystem;
+            svc.StartType = ServiceStartMode.Automatic;
+            svc.DisplayName = "SE IPA CyberLab SysLog";
+            svc.ServiceName = "syslog";
+            svc.Description = "SE IPA CyberLab SysLog Service";
+            this.Installers.Add(svc);
+            this.Installers.Add(proc);
+        }
+    }
+
+    public class SysLogService : ServiceBase
+    {
+        object lockObj = new object();
+        SysLogSvc s = null;
+
+        public SysLogService()
+        {
         }
 
         protected override void OnStart(string[] args)
         {
+            lock (lockObj)
+            {
+                if (s == null)
+                {
+                    s = new SysLogSvc();
+                }
+            }
         }
 
         protected override void OnStop()
         {
+            lock (lockObj)
+            {
+                if (s != null)
+                {
+                    s.Stop();
+                    s = null;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+                {
+                    new SysLogService()
+                };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
